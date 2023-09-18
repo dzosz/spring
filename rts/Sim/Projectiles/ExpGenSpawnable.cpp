2629c7734d361c1633efe8e95e17bc4eb9cd5ef9
dzosz <tomek3319@gmail.com>
2023-09-18 21:31:43 +0200

    fix missing order data for legacy projectiles (for sorting)

--- rts/Sim/Projectiles/ExpGenSpawnable.cpp
+++ rts/Sim/Projectiles/ExpGenSpawnable.cpp
@@ -253,11 +253,11 @@ void CExpGenSpawnable::AddEffectsQuad(const VA_TYPE_TC& tl, const VA_TYPE_TC& tr
 		{ bl.pos, float3{ bl.s, bl.t, layer }, uvInfo, animInfo, bl.c }
 	);
 	
-	
+	// TODO we don't always have to add the order. e.g. shadows aren't ordered
 	uint64_t order (static_cast<uint32_t>(drawOrder) << 31 | static_cast<uint32_t>(-sortDist));
 	//rb.AddQuadOrder(order);
 	//std::pair order{drawOrder, -sortDist};
-	//enqueuedProjectilesDrawOrderData.push_back(order);
+	enqueuedProjectilesDrawOrderData.push_back(order);
 	
 
 }
